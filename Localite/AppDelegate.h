c861995495de33c4e894336cb413ae4b0b3d54d8
Jedd Goble <jeddgoble@gmail.com>
2015-10-15 13:02:39 -0700

    Initial Commit

---
+++ Localite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Localite
+//
+//  Created by Jedd Goble on 10/15/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Localite/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Localite
+//
+//  Created by Jedd Goble on 10/15/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
